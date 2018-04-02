713ec4ea82d6b8ae7f8e1ca3ab0dd804124d8379
Jason Ekstrand <jason.ekstrand@intel.com>
2018-04-02 16:44:40 -0700

    nir: Remove deref chain support from opt_undef

--- src/compiler/nir/nir_opt_undef.c
+++ src/compiler/nir/nir_opt_undef.c
@@ -109,7 +109,6 @@ opt_undef_store(nir_intrinsic_instr *intrin)
 {
    switch (intrin->intrinsic) {
    case nir_intrinsic_store_deref:
-   case nir_intrinsic_store_var:
    case nir_intrinsic_store_output:
    case nir_intrinsic_store_per_vertex_output:
    case nir_intrinsic_store_ssbo:
@@ -133,6 +132,8 @@ nir_opt_undef(nir_shader *shader)
 {
    nir_builder b;
    bool progress = false;
+
+   nir_assert_unlowered_derefs(shader, nir_lower_load_store_derefs);
 
    nir_foreach_function(function, shader) {
       if (function->impl) {

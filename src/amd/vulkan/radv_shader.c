b572fe5ed9b6e7f31dea0f9ac7aeb78a9fc21d5f
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2018-06-03 03:12:01 +0200

    radv: Disable lower_io_to_temporaries during deref changes.
    The ordering of passes is such that this already generates deref_var
    based stores before the lower_io_to_scalar pass is ready for it.

--- src/amd/vulkan/radv_shader.c
+++ src/amd/vulkan/radv_shader.c
@@ -278,6 +278,8 @@ radv_shader_compile_to_nir(struct radv_device *device,
 
 	nir_lower_vars_to_ssa(nir);
 
+	/* Temporarily disabled during deref changes */
+#if 0
 	if (nir->info.stage == MESA_SHADER_VERTEX ||
 	    nir->info.stage == MESA_SHADER_GEOMETRY) {
 		NIR_PASS_V(nir, nir_lower_io_to_temporaries,
@@ -287,6 +289,7 @@ radv_shader_compile_to_nir(struct radv_device *device,
 		NIR_PASS_V(nir, nir_lower_io_to_temporaries,
 			   nir_shader_get_entrypoint(nir), true, false);
 	}
+#endif
 
 	nir_split_var_copies(nir);
 	nir_lower_var_copies(nir);

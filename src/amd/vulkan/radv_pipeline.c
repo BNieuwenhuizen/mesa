a557d34d1947dcbd6dabeb8f15212625b3c9e056
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2018-10-20 00:22:31 +0200

    radv: Add bool to int32 lowering

--- src/amd/vulkan/radv_pipeline.c
+++ src/amd/vulkan/radv_pipeline.c
@@ -2087,6 +2087,9 @@ void radv_create_shaders(struct radv_pipeline *pipeline,
 		radv_link_shaders(pipeline, nir);
 
 	for (int i = 0; i < MESA_SHADER_STAGES; ++i) {
+		if (nir[i])
+			nir_lower_bool_to_int32(nir[i]);
+
 		if (radv_can_dump_shader(device, modules[i], false))
 			nir_print_shader(nir[i], stderr);
 	}
